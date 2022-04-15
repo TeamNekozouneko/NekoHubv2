82c33bfaa0e4bffb4a70509ba5f7a629395cbf43
Taitaitatata <taitaitatatasub@outlook.jp>
2022-04-15 16:29:11 +0900

    [Update] ハブコマンド実装

--- src/main/java/com/nekozouneko/nekohubv2/bungee/cmd/hub.java
+++ src/main/java/com/nekozouneko/nekohubv2/bungee/cmd/hub.java
@@ -1,9 +1,19 @@
 package com.nekozouneko.nekohubv2.bungee.cmd;
 
+import com.nekozouneko.nekohubv2.bungee.NekoHubv2;
+import com.nekozouneko.nplib.chat.ChatCode;
 import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.config.ListenerInfo;
+import net.md_5.bungee.api.config.ServerInfo;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
 
+import java.util.concurrent.atomic.AtomicReference;
+
 public class hub extends Command {
+
+    private final NekoHubv2 instance = NekoHubv2.getInstance();
 
     public hub(String cmdName, String... aliases) {
         super(cmdName, null, aliases);
@@ -11,5 +21,21 @@ public class hub extends Command {
 
     @Override
     public void execute(CommandSender sender, String[] args) {
+
+        ListenerInfo listener = (ListenerInfo) instance.getProxy().getConfig().getListeners().toArray()[0];
+        ServerInfo ConnectableServ = instance.getProxy().getServerInfo(listener.getServerPriority().get(0));
+
+        ProxiedPlayer player = (ProxiedPlayer) sender;
+
+        if (ConnectableServ == null) {
+            sender.sendMessage(new TextComponent(ChatCode.RED+"ロビーサーバーがオフラインの可能性があります。"));
+            return;
+        }
+        if (player == null) {
+            sender.sendMessage(new TextComponent(ChatCode.RED+"プレイヤーとしてのみ実行可能です。"));
+        }
+
+        player.connect(ConnectableServ);
+
     }
 }
