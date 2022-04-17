f6508397e10db5c51abd329d444b65b5069b5d3a
Taitaitatata <taitaitatatasub@outlook.jp>
2022-04-17 16:51:05 +0900

    [Add] 棒を左クリックした時と右クリックした時の処理用のリスナー作成

---
+++ src/main/java/com/nekozouneko/nekohubv2/bukkit/listener/PlayerInteract.java
@@ -0,0 +1,50 @@
+package com.nekozouneko.nekohubv2.bukkit.listener;
+
+import com.nekozouneko.nekohubv2.bukkit.NekoHubv2;
+import com.nekozouneko.nekohubv2.bukkit.cmd.StickMenu;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.*;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+public class PlayerInteract implements Listener {
+
+    private NekoHubv2 instance = NekoHubv2.getInstance();
+
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent event) {
+        Player player = event.getPlayer();
+
+        try {
+            switch (event.getItem().getType()) {
+                case STICK:
+                    openStickGUI(player, event);
+                default:
+                    return;
+            }
+        } catch (Exception ignored) {}
+    }
+
+    private void openStickGUI(Player player, PlayerInteractEvent event) {
+        if (event.getAction() == Action.LEFT_CLICK_AIR) {
+            ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+            DataOutputStream out = new DataOutputStream(bytes);
+
+            try {
+                out.writeUTF(player.getName());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            player.sendPluginMessage(instance, "nhv2:requestserverpanel", bytes.toByteArray());
+        } else if (event.getAction() == Action.RIGHT_CLICK_AIR) {
+            StickMenu.initStickMenu(player);
+        }
+    }
+
+}
