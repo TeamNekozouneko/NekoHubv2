cf342cf46434576866311f4f4ec56cacefc956d3
Taitaitatata <taitaitatatasub@outlook.jp>
2022-05-08 21:20:36 +0900

    [Update] コマンドとチャンネル登録

--- src/main/java/com/nekozouneko/nekohubv2/bukkit/NekoHubv2.java
+++ src/main/java/com/nekozouneko/nekohubv2/bukkit/NekoHubv2.java
@@ -1,15 +1,15 @@
 package com.nekozouneko.nekohubv2.bukkit;
 
-import com.nekozouneko.nekohubv2.bukkit.cmd.Move;
-import com.nekozouneko.nekohubv2.bukkit.cmd.ServerPanel;
-import com.nekozouneko.nekohubv2.bukkit.cmd.Stick;
-import com.nekozouneko.nekohubv2.bukkit.cmd.StickMenu;
+import com.nekozouneko.nekohubv2.bukkit.cmd.*;
 import com.nekozouneko.nekohubv2.bukkit.listener.*;
 
 import com.nekozouneko.nplib.chat.ChatCode;
 
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * NekoHub v2 for bukkit / Spigot
@@ -24,6 +24,10 @@ public final class NekoHubv2 extends JavaPlugin {
      * @see org.bukkit.plugin.java.JavaPlugin
      */
     public static NekoHubv2 instance;
+
+    public static boolean linked = false;
+
+    public static List<String> servers = new ArrayList<>();
 
     /**
      * プラグインのインスタンスを取得します。
@@ -51,17 +55,24 @@ public final class NekoHubv2 extends JavaPlugin {
         getCommand("serverpanel").setExecutor(new ServerPanel());
         getCommand("stickmenu").setExecutor(new StickMenu());
         getCommand("stick").setExecutor(new Stick());
-
-        getLogger().info("Registering channels...");
-        Bukkit.getMessenger().registerOutgoingPluginChannel(this, "nhv2:move");
-        Bukkit.getMessenger().registerOutgoingPluginChannel(this, "nhv2:requestserverpanel");
-        Bukkit.getMessenger().registerIncomingPluginChannel(this, "nhv2:openseverpanel", new PluginMessageListener());
+        getCommand("enderchest").setExecutor(new EnderChest());
+        getCommand("rule").setExecutor(new Rule());
 
         getLogger().info("Registering listeners...");
         getServer().getPluginManager().registerEvents(new InventoryAction(), this);
         getServer().getPluginManager().registerEvents(new PlayerInteract(), this);
         getServer().getPluginManager().registerEvents(new PlayerJoin(), this);
         getServer().getPluginManager().registerEvents(new PlayerQuit(), this);
+
+        getLogger().info("Registering channels...");
+        Bukkit.getMessenger().registerOutgoingPluginChannel(this, "nhv2:move");
+        Bukkit.getMessenger().registerOutgoingPluginChannel(this, "nhv2:requestserverpanel");
+        Bukkit.getMessenger().registerIncomingPluginChannel(this, "nhv2:openseverpanel", new PluginMessageListener());
+        Bukkit.getMessenger().registerIncomingPluginChannel(this, "nhv2:sync", new PluginMessageListener());
+
+        getLogger().info("Loading configurations...");
+        saveDefaultConfig();
+
     }
 
     /**
