b080e6493b1a00a2fa3c0a51e2716db869302ac0
Taitaitatata <taitaitatatasub@outlook.jp>
2022-05-30 19:42:43 +0900

    [Update] /nhv2 | /bnhv2コマンド追加

---
+++ src/main/java/com/nekozouneko/nekohubv2/bukkit/cmd/Root.java
@@ -0,0 +1,60 @@
+package com.nekozouneko.nekohubv2.bukkit.cmd;
+
+import com.nekozouneko.nekohubv2.bukkit.NekoHubv2;
+
+import com.nekozouneko.nplib.chat.ChatCode;
+import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.command.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+
+public class Root implements CommandExecutor, TabCompleter {
+
+    private final NekoHubv2 instance = NekoHubv2.getInstance();
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (args.length == 0) {
+            Status(sender, command, label, args);
+        } else {
+            if (args[0].equalsIgnoreCase("status")) {
+                Status(sender, command, label, args);
+            } else if (args[0].equalsIgnoreCase("reload")) {
+                Reload(sender, command, label, args);
+            }
+        }
+
+        return true;
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        List<String> tab = new ArrayList<>();
+
+        if (args.length == 1) {
+            List<String> subCmd = Arrays.asList("status", "reload");
+
+            for (String sub : subCmd) {
+                if (sub.toLowerCase(Locale.ROOT).startsWith(args[0].toLowerCase(Locale.ROOT))) tab.add(sub);
+            }
+
+        }
+
+        return tab;
+    }
+
+    void Status(CommandSender sender, Command command, String label, String[] args) {
+        sender.sendMessage(instance.PREFIX + "NekoHubv2 " + ChatCode.GRAY + "v"+instance.getDescription().getVersion() + " (S)");
+        sender.sendMessage(instance.PREFIX + (instance.linked ? "プロキシと同期済み / " + instance.servers.size() + "個のサーバーが登録済み" : "プロキシと同期していません " + ChatCode.GRAY + "(鯖選択画面使用不可)"));
+    }
+
+    void Reload(CommandSender sender, Command command, String label, String[] args) {
+        instance.reloadConfig();
+
+        sender.sendMessage(instance.PREFIX + "設定ファイルを再読み込みしました。");
+    }
+
+}
---
+++ src/main/java/com/nekozouneko/nekohubv2/bungee/cmd/Root.java
@@ -0,0 +1,85 @@
+package com.nekozouneko.nekohubv2.bungee.cmd;
+
+import com.nekozouneko.nekohubv2.bungee.NekoHubv2;
+import com.nekozouneko.nplib.chat.ChatCode;
+import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.plugin.Command;
+import net.md_5.bungee.api.plugin.TabExecutor;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Root extends Command implements TabExecutor {
+
+    private NekoHubv2 instance = NekoHubv2.getInstance();
+
+    public Root(String name, String... aliases) {
+        super(name, "NekoHubv2.cmd.NekoHubv2", aliases);
+    }
+
+    @Override
+    public void execute(CommandSender sender, String[] args) {
+        if (args.length == 0) {
+            Status(sender, args);
+        } else {
+            if (args[0].equalsIgnoreCase("status")) {
+                Status(sender, args);
+            } else if (args[0].equalsIgnoreCase("sync")) {
+                Sync(sender, args);
+            }
+        }
+    }
+
+    @Override
+    public Iterable<String> onTabComplete(CommandSender sender, String[] args) {
+        List<String> tab = new ArrayList<>();
+        List<String> SUB_COMMANDS = Arrays.asList("status", "sync");
+
+        if (args.length == 1) {
+            for (String s : SUB_COMMANDS) {
+                if (s.toLowerCase().startsWith(args[0].toLowerCase())) {
+                    tab.add(s);
+                }
+            }
+        }
+
+        return tab;
+    }
+
+    void Status(CommandSender sender, String[] args) {
+        sender.sendMessage(new TextComponent(instance.PREFIX + "NekoHubv2 " + ChatCode.GRAY + "v"+instance.getDescription().getVersion() + " (B)"));
+    }
+
+    void Sync(CommandSender sender, String[] args) {
+        sender.sendMessage(new TextComponent(instance.PREFIX + "同期中です..."));
+        instance.getLogger().info("Server data is being sent...");
+
+        ByteArrayOutputStream bytes1 = new ByteArrayOutputStream();
+        DataOutputStream out1 = new DataOutputStream(bytes1);
+
+        try {
+            String servers = "";
+            for (String K : instance.getProxy().getServers().keySet()) {
+                servers += K + ";";
+            }
+
+            out1.writeUTF(servers);
+        } catch (IOException er) {
+            er.printStackTrace();
+            sender.sendMessage(new TextComponent(instance.PREFIX + ChatCode.RED + "同期に失敗しました。"));
+            return;
+        }
+
+        for (String key : instance.getProxy().getServers().keySet()) {
+            instance.getProxy().getServers().get(key).sendData("nhv2:sync", bytes1.toByteArray());
+        }
+
+        instance.getLogger().info("Sent.");
+        sender.sendMessage(new TextComponent(instance.PREFIX + "同期が完了しました。"));
+    }
+}
