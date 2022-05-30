faac4790d975100f79a0cd44873cf72073207aea
Taitaitatata <taitaitatatasub@outlook.jp>
2022-05-30 19:43:28 +0900

    [Add] 寄付者のための浮遊コマンドを作っておくができてない

---
+++ src/main/java/com/nekozouneko/nekohubv2/bukkit/cmd/Fly.java
@@ -0,0 +1,21 @@
+package com.nekozouneko.nekohubv2.bukkit.cmd;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Fly implements CommandExecutor, TabCompleter {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        return true;
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        return new ArrayList<>();
+    }
+}
